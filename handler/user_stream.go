d2c23cecf99c9eee1afac2cf9c2dce98e512e01c
Mateja <mail@matejamaric.com>
2024-01-11 11:28:53 +0100

    Revert "feat: unsuccessful exponential backoff"
    This reverts commit ff91e0c3e24107eb2be06c897674eac385e18a99.

--- handler/user_stream.go
+++ handler/user_stream.go
@@ -4,11 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"log/slog"
-	"time"
 
 	"github.com/EventStore/EventStore-Client-Go/esdb"
-	"github.com/cenkalti/backoff/v4"
-
 	"github.com/MatejaMaric/esdb-playground/db"
 	"github.com/MatejaMaric/esdb-playground/events"
 	"github.com/MatejaMaric/esdb-playground/projections"
@@ -46,34 +43,24 @@ func HandleUserStream(ctx context.Context, logger *slog.Logger, esdbClient *esdb
 		return nil
 	}
 
-	eb := backoff.NewExponentialBackOff()
-	eb.MaxElapsedTime = 3 * time.Second
+	for running := true; running; {
+		select {
+		case <-ctx.Done():
+			break
+		default:
+			opts := esdb.SubscribeToAllOptions{
+				From: lastProcessedEvent,
+				Filter: &esdb.SubscriptionFilter{
+					Type:     esdb.StreamFilterType,
+					Prefixes: []string{string(events.UserEventsStream)},
+				},
+			}
 
-	var op backoff.Operation = func() error {
-		opts := esdb.SubscribeToAllOptions{
-			From: lastProcessedEvent,
-			Filter: &esdb.SubscriptionFilter{
-				Type:     esdb.StreamFilterType,
-				Prefixes: []string{string(events.UserEventsStream)},
-			},
+			if err := db.HandleAllStream(ctx, esdbClient, opts, handler); err != nil {
+				logger.Error("handling all stream returned an error", "error", err)
+			}
 		}
-
-		return db.HandleAllStream(ctx, esdbClient, opts, handler)
 	}
 
-	var notify backoff.Notify = func(err error, d time.Duration) {
-		logger.Warn("HandleAllStream return an error, retrying",
-			"error", err,
-			"duration", d,
-		)
-	}
-
-	/*
-		`backoff.RetryNotify` most probably doesn't work with long
-		living (blocking) functions. If I remember correctly, if the
-		error happens after successful start (for example, after 12s of
-		successful event processing), it's not event going to return en
-		error, only hang.
-	*/
-	return backoff.RetryNotify(op, backoff.WithContext(eb, ctx), notify)
+	return nil
 }
